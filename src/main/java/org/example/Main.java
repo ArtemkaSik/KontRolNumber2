f4c556f36409f4f35a7bbf68cdc540f6d0d4c7af
ArtemkaSik <sikachyovartem@yandex.ru>
2024-05-25 13:44:51 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String filePath = "D:\\КФУ(Java)\\Programming\\KRnumber2\\src\\main\\java\\org\\example\\v21.png";
+        File file = new File(filePath);
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file);){
+
+            File fileDir = new File("D:\\КФУ(Java)\\Programming\\KRnumber2\\src\\main\\cw2\\v21");
+            String[] filenames = fileDir.list();
+            ArrayList<PngProcessor> threads = new ArrayList<>();
+
+            for (String filename : filenames) {
+                PngProcessor thread = new PngProcessor("D:\\КФУ(Java)\\Programming\\KRnumber2\\src\\main\\cw2\\v21\\" + filename);
+                thread.run();
+                threads.add(thread);
+            }
+            for (Thread thread:threads){
+                try{
+                    thread.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+
+            Collections.sort(threads, new Comparator<PngProcessor>() {
+                public int compare(PngProcessor thread1, PngProcessor thread2) {
+                    if (thread1.part > thread2.part) {
+                        return 1;
+                    }
+                    return -1;
+                }
+            });
+
+            for (PngProcessor thread:threads){
+                fileOutputStream.write(thread.getData());
+                fileOutputStream.flush();
+            }
+
+        } catch (RuntimeException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/example/PngProcessor.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.io.*;
+
+public class PngProcessor extends Thread {
+
+    private String filename;
+    public int part;
+    private byte[] data;
+
+    public PngProcessor(String filename){
+        this.filename = filename;
+    }
+
+    @Override
+    public void run() {
+        try (DataInputStream is = new DataInputStream(new FileInputStream(filename))) {
+
+            int sz = is.readInt();
+            data = is.readNBytes(sz);
+            int even = is.readInt();
+            part = is.readInt();
+            int finalNumber = 0;
+
+            for(byte b: data){
+                for (int i = 0; i < 8; i ++){
+                    finalNumber += (b & ( 1 << i)) >> i;
+                }
+            }
+
+            if(finalNumber % 2 != even) {
+                throw new IOException();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public int getPart() {
+        return part;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+}
